4116fa251a83cbb6a52371782e4b3b18769ac733
Troy Sargent <troyjsargent@gmail.com>
2020-07-29 22:34:32 -0500

    added persistent hour tasks on reload from local mem

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+let hourTaskList = [];
+
 //render page and pull from local memory
 function renderPage() {
+    hourTaskList = JSON.parse(localStorage.getItem("hourTaskList"));
 
+    if (hourTaskList == null) {
+        hourTaskList = [];
+    };
+
+    //load each task into time blocks
+    for (var i = 0; i < hourTaskList.length; i++) {
+        let hourValue = parseInt(hourTaskList[i].currentHourValue);
+        let task = hourTaskList[i].currentText;
+        $(".textarea").eq(hourValue).val(task);
+    };
 };
 
 // when save is clicked, add to local memory for that hour
-var hourTask = [];
-
 $(".saveBtn").on("click", function saveHourText() {
     let currentHourValue = this.id;
     let currentText = $(".textarea").eq(currentHourValue).val();
     console.log(currentText);
-    hourTask.push({currentHourValue, currentText});
-    localStorage.setItem("hourTask", JSON.stringify(hourTask));
+    hourTaskList.push({
+        currentHourValue,
+        currentText
+    });
+    localStorage.setItem("hourTaskList", JSON.stringify(hourTaskList));
 });
 
 
@@ -20,9 +34,9 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 // format color of text area conditionally based on if hour is past, present, or future
 
-$( ".textarea" ).each(function(index, value ) {
+$(".textarea").each(function (index, value) {
     let textHour = $(value).attr("value");
-    let currentHour =  moment().hour();
+    let currentHour = moment().hour();
     if (textHour < currentHour) {
         $(this).addClass("past");
     } else if (textHour == currentHour) {
@@ -31,3 +45,5 @@ $( ".textarea" ).each(function(index, value ) {
         $(this).addClass("future");
     };
 });
+
+renderPage();
